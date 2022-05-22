a03311edbe61fb3530b1269131fb3fe1ef379ea0
Snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-05-21 18:42:52 -0700

    Changed detection method from object size to base type.

--- RunicMonsterHighlighter.cs
+++ RunicMonsterHighlighter.cs
@@ -1,62 +1,57 @@
-using ExileCore;
-using ExileCore.PoEMemory.Components;
-using ExileCore.PoEMemory.MemoryObjects;
-using SharpDX;
-using System.Collections.Generic;
-
 namespace RunicMonsterHighlighter
 {
+    using System.Collections.Generic;
+    using ExileCore;
+    using ExileCore.PoEMemory.Components;
+    using ExileCore.PoEMemory.MemoryObjects;
+    using SharpDX;
+
     public class RunicMonsterHighlighter : BaseSettingsPlugin<RunicMonsterHighlighterSettings>
     {
-        private readonly Dictionary<Entity, float> _HighlightedEntities = new Dictionary<Entity, float>();
+        private readonly HashSet<Entity> _HighlightedEntities = new HashSet<Entity>();
 
         public override void EntityAdded(Entity entity)
         {
-            if (entity != null && entity.HasComponent<Positioned>() && entity.Metadata.EndsWith("ExpeditionMarker"))
-            {
-                _HighlightedEntities.Add(entity, entity.GetComponent<Positioned>().Scale);
-            }
+            if (entity != null
+             && entity.HasComponent<Positioned>()
+             && entity.Metadata.EndsWith("ExpeditionMarker")
+             && entity.HasComponent<Animated>()
+             && entity.GetComponent<Animated>().BaseAnimatedObjectEntity.Metadata.Contains("elitemarker"))
+                this._HighlightedEntities.Add(entity);
         }
 
         public override void EntityRemoved(Entity entity)
         {
-            if (_HighlightedEntities.ContainsKey(entity))
-            {
-                _HighlightedEntities.Remove(entity);
-            }
+            this._HighlightedEntities.Remove(entity);
         }
 
         public override void AreaChange(AreaInstance area)
         {
             base.AreaChange(area);
-            _HighlightedEntities.Clear();
+            this._HighlightedEntities.Clear();
         }
 
         public override void Render()
         {
-            RemoveInvalidEntities();
-            foreach (var highlightedEntity in _HighlightedEntities)
-            {
-                if (highlightedEntity.Value < Settings.Scale.Value)
-                {
-                    continue;
-                }
+            this.RemoveInvalidEntities();
 
-                var color = Settings.BorderColor.Value;
-                var size = Settings.Size.Value;
-                DrawEntity(highlightedEntity.Key, color, size);
+            foreach (var highlightedEntity in this._HighlightedEntities)
+            {
+                var color = this.Settings.BorderColor.Value;
+                var size = this.Settings.Size.Value;
+                this.DrawEntity(highlightedEntity, color, size);
             }
         }
 
         private void RemoveInvalidEntities()
         {
-            _HighlightedEntities.RemoveAllByKey(entity => !entity.IsValid);
+            this._HighlightedEntities.RemoveWhere(entity => !entity.IsValid);
         }
 
         private void DrawEntity(Entity entity, Color color, int size)
         {
-            var entityPos = GameController.IngameState.Camera.WorldToScreen(entity.Pos);
-            Graphics.DrawFrame(new RectangleF(entityPos.X, entityPos.Y, size, size), color, size);
+            var entityPos = this.GameController.IngameState.Camera.WorldToScreen(entity.Pos);
+            this.Graphics.DrawFrame(new RectangleF(entityPos.X, entityPos.Y, size, size), color, size);
         }
     }
 }
